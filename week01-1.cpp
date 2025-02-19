d860b6cf4f4664185b2b841a70197d6253897ad6
13750465 <ian941219@gmail.com>
2025-02-19 11:55:17 +0800

    week01

---
+++ week01-1.cpp
@@ -0,0 +1,17 @@
+/// week01-1.cpp 瘋狂程式 兩數相加
+/// File-New empty 開新的檔案
+#include <stdio.h>///檔名不能寫錯
+/// STDIO 的意思是 標準的standard input/output
+/// 有了 stdio.h 的外掛，才能用 scanf() printf()
+
+int main()
+{
+	int a, b;
+	scanf( "%d" , &a );
+	scanf( "%d" , &b );
+
+	printf("%d", a+b );
+
+
+}
+
