972ec6d485eb5ecc5ba09690fde94386e9f3fe49
13750465 <ian941219@gmail.com>
2025-04-30 12:01:47 +0800

    week11

---
+++ week11-4.cpp
@@ -0,0 +1,19 @@
+/// week11-4.cpp
+/// leetcode 1295. Find Numbers with Even Number of Digits
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int ans = 0;
+        for(int i=0; i<nums.size(); i++){
+            int now = nums[i];
+
+            int digits = 0;
+            while(now>0){
+                digits++;
+                now = now / 10;
+            }
+            if(digits%2==0) ans++;
+        }
+        return ans;
+    }
+};
